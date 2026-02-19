d913d38dc91700a4ebfe423a62af776900e9dff2
rm-rs <pro.ahmedigalla@gmail.com>
2026-02-19 18:16:38 +0100

    update

--- frontend/src/SignatureCapture/index.ts
+++
@@ -1,3 +0,0 @@
-// Unified SignatureCapture component
-export { default as SignatureCapture } from './SignatureCapture';
-export type { SignatureCaptureProps, SignatureData } from './SignatureCapture';
